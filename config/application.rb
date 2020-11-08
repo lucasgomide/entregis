4714f87870e969b68ec721cf2b2746e1da61b993
lucasgomide <lucaslg200@gmail.com>
2020-11-08 04:07:16 -0300

    Generating rspec files after generate model

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module Entregis
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework  :rspec, fixture: false
+      g.helper_specs    false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
