20bd7da052e1d4dddcb9ff62c64cf34a19ab2751
lucasgomide <lucaslg200@gmail.com>
2020-11-08 04:06:12 -0300

    Adding money-rails gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'dry/system/stubs'
 require 'rspec/rails'
 require 'webmock/rspec'
+require 'money-rails/test_helpers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
