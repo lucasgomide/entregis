51f9e3a343c823d1c1dbc63e186fc5bb3cfd912d
lucasgomide <lucaslg200@gmail.com>
2020-11-09 23:22:14 -0300

    Add paper_trail gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require 'dry/system/stubs'
 require 'rspec/rails'
 require 'webmock/rspec'
 require 'money-rails/test_helpers'
+require 'paper_trail/frameworks/rspec'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
