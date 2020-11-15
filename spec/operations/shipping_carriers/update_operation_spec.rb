407c80f2dd6629f2d8368eb3c4c588df4d0744e6
lucasgomide <lucaslg200@gmail.com>
2020-11-15 07:16:56 -0300

    Adding endpoint to update ShippingCarrier

---
+++ spec/operations/shipping_carriers/update_operation_spec.rb
@@ -0,0 +1,68 @@
+RSpec.describe ShippingCarriers::UpdateOperation, type: :operation do
+  subject(:operation) { described_class.new }
+  let(:updating_contract) { instance_spy(ShippingCarriers::UpdatingContract) }
+
+  let(:app_container_stubs) do
+    Entregis::Container.stub('shipping_carriers.updating_contract', updating_contract)
+  end
+
+  describe '.call' do
+    subject(:call) { operation.call(input) }
+    let(:resource) { create(:shipping_carrier) }
+
+    let(:input) do
+      {
+        id: resource.id,
+        name: 'New name',
+        document: '000000',
+        ignored_param: false
+      }
+    end
+    let(:valid_input) { input.except(:ignored_param) }
+
+    let(:contract_result) { validation_contract(input: valid_input) }
+
+    before do
+      allow(updating_contract).to receive(:call).and_return(contract_result)
+    end
+
+    it do
+      call
+      expect(updating_contract).to have_received(:call).with(input)
+    end
+
+    context 'when contract validation has failed' do
+      let(:contract_result) { validation_contract(success: false) }
+
+      it { is_expected.to be_failed.with_instance_of(Dry::Validation::Result) }
+    end
+
+    context 'when resource was not found' do
+      let(:input) { { id: resource.id + 1 } }
+
+      its(:to_result) do
+        is_expected.to be_failed.with_instance_of(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'when resource updating has failed' do
+      let(:input) { { id: resource.id, name: nil } }
+
+      its(:to_result) do
+        is_expected.to be_failed.with_instance_of(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context 'when resource was successful updated' do
+      it { is_expected.to be_success.with(resource) }
+
+      it do
+        expect { call }.to change { resource.reload.name }.to(input[:name])
+      end
+
+      it do
+        expect { call }.to change { resource.reload.document }.to(input[:document])
+      end
+    end
+  end
+end
