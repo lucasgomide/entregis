20b1797c4616ad4445a31be9b19b642ab534c65f
lucasgomide <lucaslg200@gmail.com>
2020-11-16 20:06:33 -0300

    Creating histories tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_064521) do
+ActiveRecord::Schema.define(version: 2020_11_16_225919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,17 @@ ActiveRecord::Schema.define(version: 2020_11_08_064521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["shipment_mode_id"], name: "index_vehicles_on_shipment_mode_id"
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.text "object_changes"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "carriers", "shipping_carriers"
