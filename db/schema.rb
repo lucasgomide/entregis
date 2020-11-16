63a464355a8132bb49f1094bffef9a64c6d8cf0e
lucasgomide <lucaslg200@gmail.com>
2020-11-16 20:12:25 -0300

    Updating cube_factor data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_225919) do
+ActiveRecord::Schema.define(version: 2020_11_16_230438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_225919) do
 
   create_table "shipment_modes", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "cube_factor", null: false
+    t.float "cube_factor", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
