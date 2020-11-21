8779d03b1e14a924e2282cf31262765d2c81d0cf
lucasgomide <lucaslg200@gmail.com>
2020-11-21 20:59:50 -0300

    Add endpoint to create an item of freight

--- spec/controllers/freight_items_controller_spec.rb
+++ spec/controllers/freight_items_controller_spec.rb
@@ -3,12 +3,14 @@
 RSpec.describe V1::FreightItemsController, type: :controller do
   let(:detail_operation) { instance_spy(FreightItems::DetailOperation) }
   let(:destroy_operation) { instance_spy(FreightItems::DestroyOperation) }
+  let(:create_operation) { instance_spy(FreightItems::CreateOperation) }
 
   let(:error_factory) { instance_spy(ErrorFactory) }
 
   let(:app_container_stubs) do
     Entregis::Container.stub('freight_items.detail_operation', detail_operation)
     Entregis::Container.stub('freight_items.destroy_operation', destroy_operation)
+    Entregis::Container.stub('freight_items.create_operation', create_operation)
     Entregis::Container.stub('error_factory', error_factory)
   end
 
@@ -36,5 +38,18 @@ RSpec.describe V1::FreightItemsController, type: :controller do
     end
 
     include_examples 'DELETE destroy resource'
+  end
+
+  describe 'POST create' do
+    subject(:post_create) { post :create, params: params, format: :json }
+    let(:params) { { 'freight_id': '1' } }
+    let(:resource) { create(:freight_item) }
+    let(:serializer) { V1::FreightItemSerializer }
+
+    before do
+      allow(create_operation).to receive(:call).and_return(result)
+    end
+
+    include_examples 'POST create resource'
   end
 end
