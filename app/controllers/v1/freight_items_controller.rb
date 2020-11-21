5536d347863ae1845521bf0709642e684544688d
lucasgomide <lucaslg200@gmail.com>
2020-11-21 20:18:12 -0300

    Adding endpoint to retrive info about freight item

---
+++ app/controllers/v1/freight_items_controller.rb
@@ -0,0 +1,12 @@
+module V1
+  class FreightItemsController < ApplicationController
+    include BaseController
+
+    def show
+      operation do |m|
+        m.success(&method(:render_success))
+        m.failure(&method(:render_invalid))
+      end
+    end
+  end
+end
