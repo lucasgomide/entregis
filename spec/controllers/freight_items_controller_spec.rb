8f3000891448e022f35dd69651ed16fa15938efc
lucasgomide <lucaslg200@gmail.com>
2020-11-21 20:33:10 -0300

    Add endpoint to remove an item of freight

--- spec/controllers/freight_items_controller_spec.rb
+++ spec/controllers/freight_items_controller_spec.rb
@@ -2,11 +2,13 @@
 
 RSpec.describe V1::FreightItemsController, type: :controller do
   let(:detail_operation) { instance_spy(FreightItems::DetailOperation) }
+  let(:destroy_operation) { instance_spy(FreightItems::DestroyOperation) }
 
   let(:error_factory) { instance_spy(ErrorFactory) }
 
   let(:app_container_stubs) do
     Entregis::Container.stub('freight_items.detail_operation', detail_operation)
+    Entregis::Container.stub('freight_items.destroy_operation', destroy_operation)
     Entregis::Container.stub('error_factory', error_factory)
   end
 
@@ -21,5 +23,18 @@ RSpec.describe V1::FreightItemsController, type: :controller do
     end
 
     include_examples 'GET show resource'
+  end
+
+  describe 'DELETE destroy' do
+    subject(:delete_destroy) { delete :destroy, params: params, format: :json }
+    let(:params) { { 'id': '1', 'freight_id': '1' } }
+    let(:resource) { create(:freight_item) }
+    let(:serializer) { V1::FreightItemSerializer }
+
+    before do
+      allow(destroy_operation).to receive(:call).and_return(result)
+    end
+
+    include_examples 'DELETE destroy resource'
   end
 end
