c514c315837c939158d2561993c54a8ac65ed690
lucasgomide <lucaslg200@gmail.com>
2020-11-22 15:15:36 -0300

    Avoiding duplicated query to get freight's items

--- app/operations/freight_items/update_freight_total_operation.rb
+++ app/operations/freight_items/update_freight_total_operation.rb
@@ -7,9 +7,10 @@ module FreightItems
     # For each action on freight's item, the parent freight is updated.
     def call(item)
       item.freight.tap do |freight|
+        items = freight.items
         freight.update!(
-          cubic_meters_total: freight.items.sum(&:cubic_meters),
-          weight_total: freight.items.sum(&:weight)
+          cubic_meters_total: items.sum(&:cubic_meters),
+          weight_total: items.sum(&:weight)
         )
       end
     end
