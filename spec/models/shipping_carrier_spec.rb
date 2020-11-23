c7efb32d75209390017fea3a44231ed364dd6f4d
lucasgomide <lucaslg200@gmail.com>
2020-11-23 15:39:01 -0300

    Allow to cascade delete carriers association when a shipping_carrier is deleting
    In the future, the carriers can not be deleted if any shipment has associated.

--- spec/models/shipping_carrier_spec.rb
+++ spec/models/shipping_carrier_spec.rb
@@ -2,5 +2,5 @@ RSpec.describe ShippingCarrier, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:document) }
 
-  it { is_expected.to have_many(:carriers) }
+  it { is_expected.to have_many(:carriers).dependent(:destroy) }
 end
