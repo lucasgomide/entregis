e2c8bf2b98245a2ecaa7cd1be39ac3fd23d0d6ab
lucasgomide <lucaslg200@gmail.com>
2020-11-23 16:03:58 -0300

    Fix Cheapest Carrier filter not considering the distance
    The "cheapest carrier filter" MUST consider the distance between the freight's origin and carrier's current location, thus multiple the carrier KM price.
    
    The previous filter was filtering only by km_price. This commit has changed this filter.

--- app/filters/freights/cheapest_filter.rb
+++ app/filters/freights/cheapest_filter.rb
@@ -6,15 +6,22 @@ module Freights
       base_filter: 'freights.search_base_filter',
     ]
     # This method runs the base_filter (to which some default rules are applied).
-    # After calling the base_filter the result is sorted by price per KM.
+    # After calling the base_filter the result is sorted by
+    # price per KM * distance in meters (linear). That means the cheapest carrier per KM.
+
     # That might be one of such important business rules. How to calculate the
     # tax to ship the freight. For now, it's ok to use the price per KM,
     # but there are others pricing models, such as: calculate the freight's KG,
     # share all tax for each customer (assess), etc.
     # It means that obviously, the current model is not the best. That's a big deal.
+
     def call(freight)
+      query = Arel.sql(
+        'ST_DistanceSphere(ST_Centroid(carriers.current_location::geometry),' \
+            "ST_GeomFromText('#{freight.origin}',4326)) * km_price_cents"
+      )
       result = yield base_filter.call(freight)
-      Success(result.order(:km_price_cents))
+      Success(result.order(query))
     end
   end
 end
