a617d6609e73002b1cff369d64e517e4749c7f78
lucasgomide <lucaslg200@gmail.com>
2020-11-23 16:03:58 -0300

    Fix Cheapest Carrier filter not considering the distance
    The "cheapest carrier filter" MUST consider the distance between the freight's origin and carrier's current location, thus multiple the carrier KM price.
    
    The previous filter was filtering only by km_price. This commit has changed this filter.

--- spec/filters/freights/cheapest_filter_spec.rb
+++ spec/filters/freights/cheapest_filter_spec.rb
@@ -8,38 +8,19 @@ RSpec.describe Freights::CheapestFilter, type: :filters do
 
   describe '.call' do
     subject(:call) { filter.call(freight) }
-    let(:origin) { [-44.327774047851555, -18.34279429196909] }
-    let(:destination) { [-44.2790, -18.4014] }
 
     let(:freight) do
       create(
         :freight,
-        origin: origin,
-        destination: destination
+        origin: [93.97705078125, 54.09806018306312],
+        destination: [93.01025390625, 54.452476553950206]
       )
     end
 
     let(:filter_result) { success(Carrier.all) }
 
     before do
-      carriers
       allow(base_filter).to receive(:call).and_return(filter_result)
-    end
-
-    let(:carriers) do
-      carrier_1
-      carrier_3
-      carrier_2
-    end
-
-    let(:carrier_1) { create(:carrier, km_price_cents: 1500) }
-
-    let(:carrier_2) { create(:carrier, km_price_cents: 1000) }
-
-    let(:carrier_3) { create(:carrier, km_price_cents: 500) }
-
-    let(:valid_carriers) do
-      [carrier_3, carrier_2, carrier_1]
     end
 
     it do
@@ -48,8 +29,107 @@ RSpec.describe Freights::CheapestFilter, type: :filters do
     end
 
     it { is_expected.to be_success }
-    it do
-      expect(call.value!.map(&:id)).to eql(valid_carriers.map(&:id))
+
+    let(:coverage_area) do
+      [[[
+        [
+          [93.92898559570312, 54.12945489870697],
+          [93.94889831542969, 54.07389422020433],
+          [94.1033935546875, 54.02249481009678],
+          [94.16175842285156, 54.05495438499805],
+          [94.1195297241211, 54.16142889682664],
+          [93.92864227294922, 54.128650202989306],
+          [93.92898559570312, 54.12945489870697]
+        ]
+      ]]]
+    end
+
+    context 'when the all carrier have the same km_price' do
+      let(:carriers) do
+        carrier_1
+        carrier_2
+        carrier_3
+      end
+
+      let(:carrier_1) do
+        create(:carrier, coverage_area: coverage_area, km_price_cents: 100,
+                         current_location: [
+                           94.25994873046875,
+                           54.13991452083357
+                         ])
+      end
+
+      let(:carrier_2) do
+        create(:carrier, coverage_area: coverage_area, km_price_cents: 100,
+                         current_location: [
+                           94.15695190429688,
+                           54.12945489870697
+                         ])
+      end
+
+      let(:carrier_3) do
+        create(:carrier, coverage_area: coverage_area, km_price_cents: 100,
+                         current_location: [
+                           94.4000244140625,
+                           54.16725797022493
+                         ])
+      end
+
+      let(:valid_carriers) do
+        [carrier_2, carrier_1, carrier_3]
+      end
+
+      before do
+        carriers
+      end
+
+      it do
+        expect(call.value!.map(&:id)).to eql(valid_carriers.map(&:id))
+      end
+    end
+
+    context 'when the closest carrier is expensive' do
+      let(:carriers) do
+        carrier_1
+        carrier_2
+        carrier_3
+      end
+
+      let(:carrier_1) do
+        create(:carrier, coverage_area: coverage_area, km_price_cents: 5000,
+                         current_location: [
+                           94.25994873046875,
+                           54.13991452083357
+                         ])
+      end
+
+      let(:carrier_2) do
+        create(:carrier, coverage_area: coverage_area, km_price_cents: 9000,
+                         current_location: [
+                           94.15695190429688,
+                           54.12945489870697
+                         ])
+      end
+
+      let(:carrier_3) do
+        create(:carrier, coverage_area: coverage_area, km_price_cents: 100,
+                         current_location: [
+                           94.4000244140625,
+                           54.16725797022493
+                         ])
+      end
+
+      let(:valid_carriers) do
+        [carrier_3, carrier_1, carrier_2]
+      end
+
+      before do
+        carriers
+      end
+
+      it do
+        expect(call.value!.map(&:id)).to eql(valid_carriers.map(&:id))
+      end
     end
   end
 end
